889dbda8c18ac062a7859e6a6cb457e7fadd2a59
volxdya <recommendation082@gmail.com>
2024-12-15 20:10:05 +0500

    added structure project

---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT: number = Number(process.env.SERVER_PORT) ?? 1234;
+  const HOST: string = process.env.SERVER_HOST ?? 'localhost';
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => {
+    console.log(`server -> http://${HOST}:${PORT}`);
+  });
+}
+bootstrap();

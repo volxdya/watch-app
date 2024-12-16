34b8d8484dd3499100571186907df8fbf04dd52e
volxdya <recommendation082@gmail.com>
2024-12-16 23:19:44 +0500

    added abstractions

--- backend/src/core/user/user.controller.ts
+++ backend/src/core/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AController } from '../../abstractions';
 

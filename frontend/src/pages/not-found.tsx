4329968b0ebf2a08d34c8b471cea050408817fe7
volxdya <recommendation082@gmail.com>
2025-01-07 17:23:57 +0500

    added nice design to not found page

--- frontend/src/pages/not-found.tsx
+++ frontend/src/pages/not-found.tsx
@@ -1,8 +1,13 @@
+import { Input } from "@nextui-org/input";
+
 export default function NotFoundPage() {
-    return (
-        <>
-            <p>Ничего не найдено.</p>
-            <input type="text" placeholder="Поиск чего-то..." />
-        </>
-    )
+  return (
+    <section className="flex flex-col items-center gap-4 py-8 md:py-10">
+      <p>Ничего не найдено.</p>
+      <p>Попробуйте что-то найти</p>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input label="Поиск" placeholder="Поиск" type="text" className="w-96" />
+      </div>
+    </section>
+  );
 }

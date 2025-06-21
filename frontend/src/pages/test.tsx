12d5289d1d10378a057a0781ce803b1002530b9e
volxdya <recommendation082@gmail.com>
2025-06-22 01:41:52 +0500

    test commit from macos

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,6 +1,8 @@
 import { usersFetch } from '@/api/User';
 import FetchWrapper from '@/components/fetch-wrapper';
 import { UsersType } from '@/types/user';
+
+// test component
 export default function Test() {
   return (
     <FetchWrapper<UsersType>
